27f610bb93defe23c05eabef777df1d85ff39152
Fl3XINS <fanaconaperafan@gmail.com>
2023-03-05 20:27:04 -0500

    comentarios

--- problema 14.cpp
+++ problema 14.cpp
@@ -3,6 +3,7 @@ using namespace std;
 bool polindromo(int num){
 	int  reverso = 0, digito, original;
     original = num;
+    //reutizamos del ejercicio 20 pero en este caso comprobamos si el numero es polindromo si es el caso retornamos true si no false
     while (num > 0) {
         digito = num % 10;
         reverso = (reverso * 10) + digito;
@@ -16,9 +17,11 @@ bool polindromo(int num){
 }
 int main(){
 	int maximo=0,n,valor1,valor2;
+	// ciclo para tomar valores de 999 hasta 100 primer valor
 	for(int i=999;i>100;i--){
-		for(int i2=999;i2>100;i2--){
+		for(int i2=999;i2>100;i2--){// ciclo para tomar valores de 999 hasta 100 segundo valor2
 			n=i*i2;
+			// comprobamos si el numero encontardo es mayor al actualmente mayor pilndromo encontrado si  no es el caso no hacer operaciones inmnecesarias
 			if(n>maximo){
 			if(polindromo(n)){
 				if(maximo<n){
--- problema 17.cpp
+++ problema 17.cpp
@@ -6,14 +6,16 @@ int main(){
 	cout<<"ingrese numero de factores del primer numero triangular con k factores +1 ";
 	cin>> pedido;
 	bool fake = true;
+	// ciclo para obtener los numeros triangularess
 	for(int i=0,i2=1;fake;++i2){
 		i=i+i2;
 		contador2=1;
+		// para cada numero triangular encontramos sus factores
 		for(int fact=1;fact<i;fact++){
-			if(i%fact==0){contador2++;
+			if(i%fact==0){contador2++;// sis es factor vamos sumando a contadores
 			}
 		}
-		if(contador2>=pedido+1){
+		if(contador2>=pedido+1){// conprobamos si el numero triangular de cada iteracion tiene los k elementos pedidos o mas
 			fake = false;
 			cout <<i<<" es el primer numero triangular con "<<pedido+1 <<" factores o mas  tiene "<<contador2;
 		}
